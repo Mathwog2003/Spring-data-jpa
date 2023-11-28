679799cc52568320e73947f96e8c0289ad224bca
mathwog2003 <gowthamselvaraj037@gmail.com>
2023-11-28 23:33:25 +0530

    created repositary for query manipulations

---
+++ jpa/src/main/java/com/jpalearn/jpa/Repositary/StudentRepositary.java
@@ -0,0 +1,7 @@
+package com.jpalearn.jpa.Repositary;
+
+import com.jpalearn.jpa.Entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepositary extends JpaRepository<Student,Long> {
+}
---
+++ jpa/src/test/java/com/jpalearn/jpa/Repositary/StudentRepositaryTest.java
@@ -0,0 +1,7 @@
+package com.jpalearn.jpa.Repositary;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentRepositaryTest {
+
+}
