37fbb692318716218952b06f43bf554141171622
mathwog2003 <gowthamselvaraj037@gmail.com>
2023-11-28 12:05:42 +0530

    added sequence genrator and generated value annotation

---
+++ jpa/src/main/java/com/jpalearn/jpa/Entity/Student.java
@@ -0,0 +1,35 @@
+package com.jpalearn.jpa.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "studentDetails")
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1;
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @Column(name = "email_address")
+    private String mailId;
+    private String guardianName;
+    private String GuardianMailId;
+    private String GuardianMobile;
+}
